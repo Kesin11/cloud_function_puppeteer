b4721cc0023cab141e1609373b79fb20c6edc6a8
Kenta Kase <kesin1202000@gmail.com>
2018-09-15 00:15:03 +0900

    remote-debuggingのpuppteerにclientが接続できることを確認

--- functions/ts/poc/puppeteer_localws.ts
+++ functions/ts/poc/puppeteer_localws.ts
@@ -1,9 +1,23 @@
 import puppeteer from 'puppeteer'
 import { RelayServer } from '../websocket/server'
+import { RelayClient } from '../websocket/client';
 
 const puppeteer_to_localserver = async() => {
   const server = await RelayServer.start()
 
   const browser = await puppeteer.connect({ browserWSEndpoint: 'ws://localhost:8080' })
 }
-puppeteer_to_localserver()
+
+const localclient_to_puppeteer = async() => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--remote-debugging-port=9222']
+  })
+  const browserWSEndpoint = browser.wsEndpoint()
+  console.log({browserWSEndpoint})
+
+  const client = await RelayClient.connect(browserWSEndpoint)
+  client.send('foo')
+}
+
+// puppeteer_to_localserver()
+localclient_to_puppeteer()
